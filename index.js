16fac38e950cfaa90405f8d5c707daff292b2411
futebol ao vivo <91692892+Nicollas77pk@users.noreply.github.com>
2025-04-23 13:15:37 -0300

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 
 // Proxy para qualquer link do servidor HTTP
 app.use('/proxy', createProxyMiddleware({
-  target: 'http://lexus.hubns.top', // <- Altere aqui para o host que você quiser
+  target: 'https://onplaytv.net', // <- Altere aqui para o host que você quiser
   changeOrigin: true,
   pathRewrite: {
     '^/proxy': '', // remove "/proxy" do início da URL
